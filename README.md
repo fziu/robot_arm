a1b1e2a30bdf849c15f97935760a406b961cb436
fzf <1092126969@qq.com>
2025-06-13 17:08:07 +0800

    v2.0

--- README.md
+++ README.md
@@ -3,7 +3,16 @@ ros2 launch test_description display_robot.launch.py
 ros2 control list_controllers
 ros2 topic pub /forward_position_controller/commands std_msgs/msg/Float64MultiArray "data: [1.0]"
 
-
+ros2 topic pub /joint_trajectory_position_controller/joint_trajectory trajectory_msgs/JointTrajectory '{
+  "joint_names": ["joint1"],
+  "points": [
+    {
+      "positions": [0.0],
+      "velocities": [0.0],
+      "time_from_start": {"sec": 2}
+    }
+  ]
+}'
 
 
 source install/setup.bash
--- test_ws/src/test_description/launch/display_robot.launch.py
+++ test_ws/src/test_description/launch/display_robot.launch.py
@@ -15,9 +15,10 @@ from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 def generate_launch_description():
     # 获取urdf、config文件
     robot_description_content = Command([
-        PathJoinSubstitution([FindExecutable(name="cat")]), " ",
-        PathJoinSubstitution([FindPackageShare("test_description"), "urdf", "test.urdf"])]
+        PathJoinSubstitution([FindExecutable(name="xacro")]), " ",
+        PathJoinSubstitution([FindPackageShare("test_description"), "urdf", "test.urdf.xacro"])]
     )
+
     robot_description = {"robot_description": robot_description_content}
 
     robot_controllers = PathJoinSubstitution(
@@ -49,10 +50,10 @@ def generate_launch_description():
         output="screen"
     )
 
-    robot_controller_spawner = Node(
+    robot_trajectory_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+        arguments=["joint_trajectory_position_controller", "--controller-manager", "/controller_manager"],
         output="screen"
     )
 
@@ -69,24 +70,24 @@ def generate_launch_description():
     )
 
     # 执行完target_action，再执行on_exit
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
-
-    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=robot_controller_spawner,
-            on_exit=[joint_state_broadcaster_spawner],
-        )
-    )
-
-    delay_robot_controller_spawner_after_spawn_entity = RegisterEventHandler(
+    delay_joint_state_broadcaster_after_spawn_entity = RegisterEventHandler(
         event_handler=OnProcessExit(
             target_action=spawn_entity,
-            on_exit=[robot_controller_spawner],
+            on_exit=[joint_state_broadcaster_spawner]
+        )
+    )
+
+    delay_trajectory_controller_after_joint_state_broadcaster = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_trajectory_controller_spawner]
+        )
+    )
+
+    delay_rviz_after_trajectory_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=robot_trajectory_controller_spawner,
+            on_exit=[rviz_node]
         )
     )
 
@@ -100,7 +101,7 @@ def generate_launch_description():
         gazebo,
         robot_state_pub_node,
         spawn_entity,
-        delay_robot_controller_spawner_after_spawn_entity,
-        delay_joint_state_broadcaster_after_robot_controller_spawner,
-        delay_rviz_after_joint_state_broadcaster_spawner
+        delay_joint_state_broadcaster_after_spawn_entity,
+        delay_trajectory_controller_after_joint_state_broadcaster,
+        delay_rviz_after_trajectory_controller
     ])
